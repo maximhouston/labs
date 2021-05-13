f365a59e810a7a0380402edfb267ca46a3da7f7d
Maxim <49352910+maximhouston@users.noreply.github.com>
2021-05-13 20:41:34 +0300

    Delete site directory

--- site/pages/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-		$link = mysqli_connect("localhost", "root", "root", "mysite");
-		  if (mysqli_connect_errno()) {
-			exit();
-			}
-?>
--- site/pages/root/navbar.php
+++
@@ -1,6 +0,0 @@
-			<ul class="navi">
-			  <a href= "/pages/page.php?page=service"><li>Каталог услуг</li></a>
-			  <a href= "/pages/page.php?page=contact"><li>Контактная информация</li></a>
-			  <a href= "/pages/page.php?page=news&num=1"><li>Новости</li></a>
-			  <a href= "/pages/page.php?page=about"><li>О клинике</li></a>
-			</ul>
